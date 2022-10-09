d639207813d0c628019544ae4ec602086a8f3c70
Dan Wager <danielwager@gmail.com>
2022-10-08 22:57:54 -0700

    Add target state

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,5 +1,5 @@
 import { AccessoryConfig, AccessoryPlugin, API, CharacteristicValue, HAP, Logging, Service } from 'homebridge';
-import { Power, WinixAPI } from './winix';
+import { Mode, Power, WinixAPI } from './winix';
 
 const winix = new WinixAPI();
 
@@ -26,24 +26,28 @@ export class WinixPurifierAccessory implements AccessoryPlugin {
 
     this.purifier = new this.hap.Service.AirPurifier(this.deviceName);
     this.purifier.getCharacteristic(this.hap.Characteristic.Active)
-      .onGet(this.getActive.bind(this))
-      .onSet(this.setActive.bind(this));
+      .onGet(this.getPower.bind(this))
+      .onSet(this.setPower.bind(this));
 
     this.purifier.getCharacteristic(this.hap.Characteristic.CurrentAirPurifierState)
       .onGet(this.getCurrentState.bind(this));
+
+    this.purifier.getCharacteristic(this.hap.Characteristic.TargetAirPurifierState)
+      .onGet(this.getMode.bind(this))
+      .onSet(this.setMode.bind(this));
   }
 
-  async getActive(): Promise<CharacteristicValue> {
+  async getPower(): Promise<CharacteristicValue> {
     const power: Power = await winix.getPower(this.deviceId);
     return power === Power.On ? this.hap.Characteristic.Active.ACTIVE : this.hap.Characteristic.Active.INACTIVE;
   }
 
-  async setActive(value: CharacteristicValue) {
-    const power: Power = value === this.hap.Characteristic.Active.ACTIVE ? Power.On : Power.Off;
+  async setPower(state: CharacteristicValue) {
+    const power: Power = state === this.hap.Characteristic.Active.ACTIVE ? Power.On : Power.Off;
     await winix.setPower(this.deviceId, power);
 
     // TODO: refactor out a way to force a status update
-    this.purifier.updateCharacteristic(this.hap.Characteristic.Active, value);
+    this.purifier.updateCharacteristic(this.hap.Characteristic.Active, state);
     // This one is necessary to force the UI to update? weird
     this.purifier.updateCharacteristic(this.hap.Characteristic.CurrentAirPurifierState, this.toCurrentState(power));
   }
@@ -54,12 +58,33 @@ export class WinixPurifierAccessory implements AccessoryPlugin {
       this.hap.Characteristic.CurrentAirPurifierState.INACTIVE;
   }
 
+  async getMode(): Promise<CharacteristicValue> {
+    const mode: Mode = await winix.getMode(this.deviceId);
+    return mode === Mode.Auto ? this.hap.Characteristic.TargetAirPurifierState.AUTO :
+      this.hap.Characteristic.TargetAirPurifierState.MANUAL;
+  }
+
+  async setMode(state: CharacteristicValue) {
+    const mode: Mode = state === this.hap.Characteristic.TargetAirPurifierState.AUTO ? Mode.Auto : Mode.Manual;
+    await winix.setMode(this.deviceId, mode);
+    this.purifier.updateCharacteristic(this.hap.Characteristic.TargetAirPurifierState, this.toTargetState(mode));
+  }
+
   private toCurrentState(power: Power): CharacteristicValue {
     switch (power) {
       case Power.On:
         return this.hap.Characteristic.CurrentAirPurifierState.PURIFYING_AIR;
       case Power.Off:
         return this.hap.Characteristic.CurrentAirPurifierState.INACTIVE;
+    }
+  }
+
+  private toTargetState(mode: Mode): CharacteristicValue {
+    switch (mode) {
+      case Mode.Auto:
+        return this.hap.Characteristic.TargetAirPurifierState.AUTO;
+      case Mode.Manual:
+        return this.hap.Characteristic.TargetAirPurifierState.MANUAL;
     }
   }
 
